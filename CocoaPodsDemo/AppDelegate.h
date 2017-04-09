b127ee082eb6fb2876ed53f44face4b44e805fd8
Johnson Zhang <chizhang1990@hotmail.com>
2017-04-09 15:47:09 +0800

    Initial Commit

---
+++ CocoaPodsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CocoaPodsDemo
+//
+//  Created by 张驰 on 2017/4/9.
+//  Copyright © 2017年 张驰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
